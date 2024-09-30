eb3b9f0482f24c08f87d41df5916424c611fe995
Me7atonin <sammyalex2006@gmail.com>
2024-09-30 12:18:27 -0700

    Objective, fixing errors, Win scene,

--- TopDownShowcase/Assets/Scripts/Player Animation.cs
+++ TopDownShowcase/Assets/Scripts/Player Animation.cs
@@ -13,7 +13,7 @@ public class PlayerAnimation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Time.timeScale == 1)
+        
         {
             float xInput = Input.GetAxis("Horizontal");
             float yInput = Input.GetAxis("Vertical");
